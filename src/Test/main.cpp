b9ea4aef4fdb54b42545b19886de0c10358a6cd6
Marien-Garon <143949086+MgPhenix@users.noreply.github.com>
2026-07-23 15:06:45 +0200

    idk

--- src/Test/main.cpp
+++ src/Test/main.cpp
@@ -1,76 +1,20 @@
 #include "include.h"
 #include <iostream>
-#include <fstream>
-#include <ctime>
-#include <thread>
-#include <mutex>
-
-class Test
-{
-private: 
-	int oui = 0;
-public:
-	Test(int oui) : oui(oui) {};
-	void PrintOui() { std::cout << oui << std::endl; };
-};
-
-int score = 0;
-std::mutex mutex_score;
-
-int task1(int a) 
-{
-	//std::lock_guard<std::mutex> lock(mutex_score);
-	std::cout << "Thread 1 is running. ID: " << std::this_thread::get_id() << "\n";
-	score += a;
-	return score + 10;
-}
-
-//auto task2() -> int
-//{
-//	//std::lock_guard<std::mutex> lock(mutex_score);
-//	std::cout << "Thread 2 is running. ID: " << std::this_thread::get_id() << "\n";
-//	score += 15;
-//}
+//#include <WinSock2.h>
 
 int main()
 {
-	//ThreadPool pool(4);
+//	WSADATA wsaData;
 
-	//auto test = pool.Submit(task1, 10);
+    //if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)
+    //    /* La fonction WSAStartup a échoué */;
+    //else
+    //{
+    //    /* ... On peut continuer ... */
 
-	//std::this_thread::sleep_for(std::chrono::milliseconds(4000));
-
-	//std::cout << "1 : " << score << std::endl;
-
-	//int score2 = test.get();
-
-	//std::cout << "1 : " << score << std::endl;
-	//std::cout << "2 : " << score2 << std::endl;
-
-	/*std::cout << "3 : " << score << std::endl;
-
-	std::cout << "4 : " << score << std::endl;*/
-
-
-	//std::this_thread::sleep_for(std::chrono::milliseconds(10));
-
-	//EventSystem& ES = EventSystem::GetInstance();
-
-	//ES.Subscribe("test", [](const EventData& data) { std::cout << "test" << event_cast<int>(data) << std::endl; });
-
-	//ES.Emit("test", 43);
-
-	//Timer timer;
-	//float64 start = timer.Start();
-
-	//Logfile& logger = Logfile::GetInstance();
-	//logger.OpenFile("../../latest.log");
-
-	//logger.SaveInFile({ Severity::WARN,"18:00:00","Ceci est un deuxieme test" });
-
-	//float64 end = timer.End();
-
-	//std::cout << "Start : " << start << " | End : " << end << " | Time : " << end - start << std::endl;
+    //    /*, Mais il faut appeler WSACleanup a la fin */
+    //    WSACleanup();
+    //}
 
 	return 0;
 }
--- src/ToolsBox/AssetEngine.h
+++ src/ToolsBox/AssetEngine.h
@@ -1,3 +1,7 @@
+/*
+WORK IN PROGRESS
+*/
+
 #pragma once
 #include <unordered_map>
 #include <vector>
