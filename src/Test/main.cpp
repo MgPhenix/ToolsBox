190b1eb01a27ae04427764a4ff8cb6d4460d2276
Marien-Garon <143949086+MgPhenix@users.noreply.github.com>
2026-07-24 15:35:13 +0200

    [ADD] Some shit test
    maybe i will stop this idea cause meh
    Idk how to do it

--- src/Test/main.cpp
+++ src/Test/main.cpp
@@ -1,10 +1,33 @@
 #include "ToolsBox.h"
 #include <iostream>
+#include <functional>
+
+template<typename F, typename... Args>
+auto AutoConvertSubmission(F&& func, Args&&... args) -> std::function<std::invoke_result_t<F, Args...>()>
+{
+	using ReturnType = std::invoke_result_t<F, Args...>;
+
+	return [f = std::forward<F>(func),
+		args = std::make_tuple(std::forward<Args>(args)...)]() mutable {
+		return std::apply(f, args);
+		};
+}
+
+int calcul(int a, int b)
+{
+	return a + b;
+}
+
+template<typename... Args>
+std::vector<std::function<void(Args...)>> funcList;
 
 int main()
 {
 
-	std::packaged_task
 
+	//auto test = Test(calcul, 4, 2);
+	//auto truc = test();
+	
+	//std::cout << truc << std::endl;
 	return 0;
 }
