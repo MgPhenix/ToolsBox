e5b84c0b5322ce16807b19e10e432867094b6b41
Marien-Garon <143949086+MgPhenix@users.noreply.github.com>
2026-07-28 16:57:45 +0200

    Just some shit test

--- src/Test/main.cpp
+++ src/Test/main.cpp
@@ -3,16 +3,132 @@
 #include <functional>
 #include <unordered_map>
 
+template<typename T>
+struct MyAllocator
+{
+	using value_type = T;
+
+	MyAllocator() = default;
+
+	T* allocate(size_t size)
+	{
+		std::cout << "Allocate(" << size << ")" << std::endl;
+		return static_cast<T*>(::operator new(size * sizeof(T)));
+	}
+
+	void deallocate(T* ptr, size_t size)
+	{
+		std::cout << "Deallocate(" << size << ")" << std::endl;
+		::operator delete(ptr, size * sizeof(T));
+	}
+
+	template<typename U>
+	struct rebind
+	{
+		using other = MyAllocator<U>;
+	};
+};
+
+template<typename T, typename Allocator = MyAllocator<T>>
+class MyVector
+{
+private:
+
+	Allocator allocator;
+	T* memory;
+
+	size_t size		= 0;
+	size_t capacity = 1;
+
+public:
+
+	MyVector()
+	{
+		memory = allocator.allocate(1);
+	}
+
+	void push_back(T&& val)
+	{
+		if (size == capacity)
+		{
+			T* new_memory = allocator.allocate(capacity * 2);
+			for (size_t i = 0; i < size; i++)
+			{
+				new (&new_memory[i]) T(std::move_if_noexcept(memory[i]));
+				memory[i].~T();
+			}
+
+			allocator.deallocate(memory, capacity);
+			memory = new_memory;
+			capacity *= 2;
+		}
+
+		new (&memory[size]) T(std::move_if_noexcept(val));
+		size++;
+	};
+
+	size_t capacity_()  { return capacity; };
+	size_t size_()		{ return size; };
+
+	T& operator[](size_t position)
+	{
+		ASSERT(position <= size);
+
+		return memory[position];
+	}
+
+	~MyVector()
+	{
+		for (size_t i = 0; i < size; i++)
+			memory[i].~T();
+
+		allocator.deallocate(memory, capacity);
+	}
+};
+
+struct Test
+{
+	int x = 42;
+	Test() { std::cout << "Test constructor" << std::endl; };
+	Test(const Test&) { std::cout << "Test copie" << std::endl; };
+	Test(Test&&) noexcept { std::cout << "Test move" << std::endl; };
+	~Test() { std::cout << "Test destructor" << std::endl; };
+};
+
+struct Node { int key; float value; };
 
 int main()
 {
-	EventSystemV2& ES = EventSystemV2::GetInstance();
+	MyAllocator<float> alloc;
 
-	ListenerID ID = ES.Subscribe("test", [](int a, int b) { return a + b; });
+	using ReboundAllocType = MyAllocator<float>::rebind<Node>::other;
+	ReboundAllocType allocNode;
 
-	Result<int> result_test = ES.Emit<int>("test", 5, 7);
+	Node* n = allocNode.allocate(1);
+	new (n) Node{ 42, 3.14f };
 
-	std::cout << result_test[ID] << std::endl; // Affiche 5 + 7 donc 12
-
+	std::cout << "Key : " << n->key << " | Value : " << n->value << std::endl;
+	n->~Node();
+	allocNode.deallocate(n, 1);
 	return 0;
 }
+
+
+
+//int main()
+//{
+//	Test* mem = static_cast<Test*>(::operator new(3 * sizeof(Test)));
+//
+//	std::cout << mem[0].x << std::endl; //Print n'importe quoi 
+//
+//	new (&mem[0]) Test();
+//
+//	std::cout << mem[0].x << std::endl; //Print n'importe quoi 
+//
+//	mem[0].~Test();
+//
+//	::operator delete(mem);
+//
+//
+//	return 0;
+//}
